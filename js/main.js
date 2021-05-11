69d451d50a40d8691be5232b94a772b020ae80a6
joeshim <shinmi1129@gmail.com>
2021-05-11 14:00:40 +0900

    first-commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+//Hamburger menu
+const target = document.getElementById("menu");
+target.addEventListener('click', () => {
+  const target = document.getElementById("menu");
+  target.classList.toggle('open');
+  const nav = document.getElementById("drawer-nav");
+  nav.classList.toggle('in');
+});
